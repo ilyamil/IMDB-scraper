e5a23336cf555f16653e7d49e119f963a69f6c52
imilyutin <idmilyutin@gmail.com>
2022-11-09 23:31:49 +0400

    added preprocessing of reviews in run_scrapper

--- run_scraper.py
+++ run_scraper.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from imdb_scraper.movie_metadata import collect_metadata
 from imdb_scraper.movie_reviews import collect_reviews
-from imdb_scraper.preprocessing import preprocess_metadata
+from imdb_scraper.preprocessing import preprocess_metadata, preprocess_reviews
 from imdb_scraper.utils import read_yaml
 
 
@@ -29,7 +29,8 @@ def run(entity: str):
     elif entity == 'reviews':
         collect_reviews(config['reviews'], credentials)
     elif entity == 'preprocessing':
-        preprocess_metadata(config['preprocessing'], credentials)
+        # preprocess_metadata(config['preprocessing'], credentials)
+        preprocess_reviews(config['preprocessing'], credentials)
     else:
         raise ValueError('Unsupported entity')
 
